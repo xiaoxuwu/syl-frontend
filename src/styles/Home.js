9f5c7c699790e3dcb66910eaf9c5d40fa97178b9
Yun Xu <iamyun@g.ucla.edu>
2019-05-27 18:15:30 -0700

    create dual column for Edit page

---
+++ src/styles/Home.js
@@ -0,0 +1,13 @@
+const HomeStyles = theme => ({
+  content: {
+  	margin: '25px 25px 80px 25px',
+    textAlign: 'center',
+  },
+  list: {
+    display: 'inline-flex',
+    padding: '25px 25px 25px 25px',
+    justifyContent: 'center'
+  },
+});
+
+export default HomeStyles;
