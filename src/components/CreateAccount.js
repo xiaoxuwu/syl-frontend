2d2fbed0367ee0f629eb46581ce7efd5a6538ff8
Carter Wu <carter.wu@ucla.edu>
2019-05-29 23:14:55 -0700

    remove errors, more polish

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -8,9 +8,12 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import InputLabel from '@material-ui/core/InputLabel';
 >>>>>>> polish
+=======
+>>>>>>> remove errors, more polish
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
